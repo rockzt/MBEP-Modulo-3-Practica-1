6e4ac6c2a4febb4755ac04491604c5e51a925eee
rodrigo zavala <rodrigo.zavala@troquer.com.mx>
2023-06-28 10:04:33 -0600

    [modified] Practica5.py

--- practicas/practica5.py
+++ practicas/practica5.py
@@ -1,8 +1,15 @@
 # Prática 5: Escribe un script que solicite un número y devuelva notifique al usuario si el número es par o impar
 print("\n")
 print("Práctica 5 Es par o impar")
-numero = float(input("Ingresa un numero: "))
-if numero % 2 == 0:
-    print("\t El número es par")
+es_numerico = False
+
+while not es_numerico:
+    numero = input('Ingresa un número: ')
+    es_numerico = numero.isnumeric()
+
+mod = int(numero) % 2
+
+if mod == 0:
+    print("El número es par")
 else:
-    print("\t El número es impar")
+    print("El número es impar")
